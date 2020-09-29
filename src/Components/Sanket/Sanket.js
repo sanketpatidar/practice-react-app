ce8da89bce295b2976acef6caa0a22bdc1b56e41
Sanket Patidar <58832751+sanketpatidar@users.noreply.github.com>
2020-09-29 15:18:08 -0400

    Create Sanket.js

---
+++ src/Components/Sanket/Sanket.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import './Sanket.css'
+
+function Sanket() {
+
+    const addNumber = function () {
+
+    }
+
+    const name = "Sanket";
+    let nickname = "Sanket1";
+
+    nickname = "Sanket2";
+
+    return (
+        <p className={"paragraph"}>
+            Hello {nickname}
+        </p>
+    );
+}
+
+export default Sanket;
