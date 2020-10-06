b0281b9fce386003e5989aab2fdc5338106ef554
Sanket Patidar <58832751+sanketpatidar@users.noreply.github.com>
2020-10-05 22:31:52 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-import Sanket from './Components/Sanket/Sanket'
+// import Sanket from './Components/Sanket/Sanket'
+// import NewComponent from './Components/NewComponent/NewComponent'
+import PhysicsCal from './Components/PhysicsCal/PhysicsCal'
+
 
 
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Sanket Patidar</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React !!!
-        </a>
+        </a> */}
 
-        <Sanket />
 
-      </header>
+      {/* <Sanket /> */}
+
+      {/* <NewComponent /> */}
+
+      <PhysicsCal />
+
+      {/* </header> */}
     </div>
   );
 }
