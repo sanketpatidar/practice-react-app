bfb617c37cdd79304fe892c468d84b5f3c18464b
Sanket Patidar <58832751+sanketpatidar@users.noreply.github.com>
2020-10-05 22:31:48 -0400

    Create NewComponent.js

---
+++ src/Components/NewComponent/NewComponent.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import './NewComponent.css'
+
+function NewComponent() {
+
+
+
+    return (
+        <p id="newComponent">Hello New Component</p>
+    );
+}
+
+export default NewComponent;
